33f4018a475c3edc01a3b87abb0febf4925718db
jimejuarezv <126489699+jimejuarezv@users.noreply.github.com>
2024-03-15 12:20:49 -0600

    Add files via upload

---
+++ js/data_web.js
@@ -0,0 +1,22 @@
+let lista_webs = [
+    {
+        nombre: "Juego de memoria 2024",
+        img: "https://i.ytimg.com/vi/n8ujo9spBR4/maxresdefault.jpg",
+        github_page: " https://jimejuarezv.github.io/JuegoMemorianuevo/",
+       
+    },
+    {
+        nombre: "Buscador",
+        img:"https://static.vecteezy.com/system/resources/previews/011/360/566/non_2x/workspace-computer-cartoon-icon-illustration-technology-business-icon-concept-isolated-premium-flat-cartoon-style-vector.jpg",
+        github_page: " https://jimejuarezv.github.io/Buscador/",
+      
+    },
+    {
+        nombre: "Galeria",
+        img:"https://onetomarket.es/wp-content/uploads/2022/05/4gLZYgmRN6E.jpg",
+        github_page: "https://jimejuarezv.github.io/GaleriaImagenes/",
+       
+    },
+];
+
+export { lista_webs };
---
+++ js/header_component.js
@@ -0,0 +1,13 @@
+let menu_header = `
+    <div class="logo"></div>
+    <nav>
+        <div>Opcion 1</div>
+        <div>Trabajos</div>
+        <div>Destacados</div>
+        <div>Opciones</div>
+        <div>Opciónes 6</div>
+    </nav>
+    <div class="btn_header">@jimejuarezv</div>
+`;
+
+export { menu_header}
